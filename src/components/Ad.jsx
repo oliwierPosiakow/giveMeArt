2a2e93b4277a0569d9b7c082e1eecdf16ee8e76f
Oliwier Posiakow <oliwierposiek@gmail.com>
2023-08-22 01:09:17 +0200

    Card

--- src/components/Ad.jsx
+++ src/components/Ad.jsx
@@ -3,19 +3,19 @@ import boy from '/boyImg.png'
 import padlockIcon from '/padlockIcon.png'
 function Ad() {
     return (
-        <div>
-            <div>
-                <p>Style preview</p>
+        <div className='w-full flex rounded-xl overflow-hidden shadow-xl mb-6'>
+            <div className='w-1/2 flex flex-col items-center bg-[#f9f9fa] p-2'>
+                <p className='font-semi-bold text-[13px]'>Style preview</p>
                 <img src={boy} alt="Boy img"/>
             </div>
-            <div>
-                <p className='uppercase'>minimalistic</p>
-                <div>
-                    <p className='uppercase'>cartoon</p>
+            <div className='w-1/2 flex flex-col items-center justify-center bg-[#f2f2f5] gap-3'>
+                <p className='uppercase font-semi-bold text-[14px]'>minimalistic</p>
+                <div className='bg-accent py-2.5 px-5 shadow-[inset_2px_2px_6px_rgba(0,0,0,0.6)] rounded-lg'>
+                    <p className='uppercase text-offwhite'>cartoon</p>
                 </div>
-                <div>
-                    <img src="" alt=""/>
-                    <p className='uppercase'>artistic</p>
+                <div className='flex justify-center items-center gap-1'>
+                    <img src={padlockIcon} alt="Padlock icon, content locked"/>
+                    <p className='uppercase text-dark-gray text-stroke]'>artistic</p>
                 </div>
             </div>
         </div>
